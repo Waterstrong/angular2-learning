f035adca6d032646f2b168c5e63c96389d3f0207
Waterstrong <sqlin@thoughtworks.com>
2016-09-01 18:12:47 +0800

    add note card

---
+++ src/app/home/notes/note-card/index.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core"
+
+@Component({
+    selector: 'note-card',
+    template: require('./note-card.html'),
+    styles: [require('./note-card.css')]
+})
+
+export class NoteCard {}
