f5b8ac1289ed7fc0034e1e93acb7b8b9bc1bb9ed
Waterstrong <sqlin@thoughtworks.com>
2016-09-08 17:46:24 +0800

    event on create note

--- src/app/home/notes/note-creator/index.ts
+++ src/app/home/notes/note-creator/index.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core"
+import {Component, Output, EventEmitter} from "@angular/core"
 
 @Component({
     selector: 'note-creator',
@@ -10,8 +10,16 @@ export class NoteCreator {
     newNote = {
         title: '',
         value: ''
-    }
+    };
+
+    @Output() onCreateNode = new EventEmitter();
+
     createNode() {
-        console.log(this.newNote)
+        const {title, value} = this.newNote;
+
+        this.onCreateNode.emit({title, value});
+
+        return false;
+
     }
 }
