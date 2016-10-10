f09dfe5339f11a60e062b885d5e817f90ba9c6b1
Waterstrong <sqlin@thoughtworks.com>
2016-10-10 21:44:37 +0800

    refactor appbar

--- src/app/shared/components/app-bar/index.ts
+++ src/app/shared/components/app-bar/index.ts
@@ -1,9 +1,14 @@
 import {Component} from "@angular/core"
+import {ROUTER_DIRECTIVES} from '@angular/router'
 
 @Component({
     selector: 'app-bar',
     template: require('./app-bar.html'),
+    directives: [
+        ...ROUTER_DIRECTIVES
+    ],
     styles: [require('./app-bar.css')]
 })
 
-export class AppBar {}
+export class AppBar {
+}
