da8fe65ae26db553c10630bf1a874283ecf07774
Waterstrong <sqlin@thoughtworks.com>
2016-10-11 22:45:10 +0800

    re-distribute routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,10 +2,17 @@ import {RouterConfig} from '@angular/router'
 import {Home} from './home'
 import {About} from './about'
 import { AuthComponent } from './auth'
+import { MainComponent } from './main'
 
-export const router: RouterConfig = [
-    {path: '', component: Home},
-    {path: 'about', component: About},
+export const router:RouterConfig = [
+    {
+        path: '',
+        component: MainComponent,
+        children: [
+            {path: '', component: Home},
+            {path: 'about', component: About},
+        ]
+    },
     {path: 'auth', component: AuthComponent},
     {path: '**', redirectTo: ''}
 ]
