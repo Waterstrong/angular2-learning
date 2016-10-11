fc21ad9f6e0a4061e7bfe4c4e44c47c435234829
Waterstrong <sqlin@thoughtworks.com>
2016-10-11 22:35:48 +0800

    create auth component and routes

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+    selector: 'auth',
+    template: require('./auth.component.html')
+})
+export class AuthComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
